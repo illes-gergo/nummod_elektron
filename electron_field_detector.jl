b3830ab86d030714f3e6f04607beb3a53a5f3d4f
Illés Gergő <illes.gergo1@gmail.com>
2023-03-01 12:50:30 +0100

    Adatok kigenerálása

--- electron_field_detector.jl
+++ electron_field_detector.jl
@@ -1,19 +1,23 @@
-using Base.Threads
+using Base.Threads, Plots
 
 include("functions_detector.jl")
 
-xStart = -1e-6;
-yStart = -2e-6;
+xStart = -1e-4;
+yStart = -2e-4;
 xEnd = -xStart;
 yEnd = -yStart;
-count = 1e3;
+count = Int(1e3);
 
 dtSensor = 1e-9;
+t = range(start=0, step=dtSensor, length=count)
 c = 3e8;
 
-sensor = createGridArray(xStart,xEnd,count,yStart,yEnd,count)
+sensor = createGridArray(xStart, xEnd, count, yStart, yEnd, count)
 
+trajectory = createTrajectory(xStart, xEnd, count)
 
+data = initDataArray(sensor, trajectory)
 
-trajectory = createTrajectory(xStart,xEnd,count)
-
+@threads for i in 1:count
+    calculateEField(trajectory[:, i], sensor, data[i, :, :, :], t[i])
+end
--- functions_detector.jl
+++ functions_detector.jl
@@ -1,5 +1,5 @@
 function createGridArray(xStart, xEnd, xCount, yStart, yEnd, yCount)
-    output = Array{Float64}(undef, (Int(yCount), Int(xCount), 2))
+    output = Array{Float64}(undef, (yCount, xCount, 2))
     xVals = range(xStart, xEnd, Int(xCount))
     yVals = range(yStart, yEnd, Int(yCount))
 
@@ -13,6 +13,19 @@ function createGridArray(xStart, xEnd, xCount, yStart, yEnd, yCount)
 end
 
 function createTrajectory(xStart, xEnd, count)
-    xVals = range(xStart,xEnd,Int(count))'
-    return cat(xVals,zeros(size(xVals)),dims=1)
+    xVals = range(xStart, xEnd, Int(count))'
+    return cat(xVals, zeros(size(xVals)), dims=1)
+end
+
+function initDataArray(sensor, trajectory)
+    return Array{Float64}(undef, (size(trajectory, 2), size(sensor, 1), size(sensor, 2), 2))
+end
+
+function calculateEField(electron_position, sensor, data, t)
+    c0 = 3e8
+    XDiff = electron_position[1] .- sensor[:, :, 1]
+    YDiff = electron_position[2] .- sensor[:, :, 2]
+    distances = (XDiff .^ 2 .+ YDiff .^ 2) .^ 0.5
+    data[:, :, 1] .= distances
+    data[:, :, 2] .= t .+ distances ./ c0
 end
