124c326b59be95df6a72f9dee4aec8f15ba6b461
Illes Gergo <illes.gergo1@gmail.com>
2023-03-21 11:04:55 +0100

    Retardáció 2

--- functions_detector.jl
+++ functions_detector.jl
@@ -1,14 +1,6 @@
 function createGridArray(xStart, xEnd, xCount, yStart, yEnd, yCount)
-    output = Array{Float64}(undef, (yCount, xCount, 2))
-    xVals = range(xStart, xEnd, Int(xCount))
-    yVals = range(yStart, yEnd, Int(yCount))
-
-    @threads for j in eachindex(xVals)
-        for i in eachindex(yVals)
-            output[i, j, 2] = yVals[i]
-            output[i, j, 1] = xVals[j]
-        end
-    end
+    xGrid, yGrid = ndgrid(range(xStart, xEnd, length=xCount), range(yStart, yEnd, length=yCount))
+    output = cat(xGrid, yGrid, dims=3)
     return output
 end
 
@@ -26,6 +18,7 @@ function calculateEField(electron_position, sensor, data, t)
     XDiff = electron_position[1] .- sensor[:, :, 1]
     YDiff = electron_position[2] .- sensor[:, :, 2]
     distances = (XDiff .^ 2 .+ YDiff .^ 2) .^ 0.5
-    data[:, :, 1] .= distances
+    data[:, :, 1] .= distances.^0.5
     data[:, :, 2] .= t .+ distances ./ c0
+    return data
 end
