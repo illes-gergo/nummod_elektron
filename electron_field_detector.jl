3e41f8cb39a8fad75222cf2ebdaadb54e4389d73
Illés Gergő <illes.gergo1@gmail.com>
2023-04-19 12:52:43 +0200

    Órai bemutatás

--- electron_field_detector.jl
+++ electron_field_detector.jl
@@ -1,27 +1,26 @@
-using Base.Threads, Plots, LazyGrids, Interpolations
+using Base.Threads, LazyGrids, Interpolations, PlotlyJS
 
 include("functions_detector.jl")
 
-default(aspect_ratio=:auto)
 
 @time begin
 
-    xStart = -1e-4
-    yStart = -1e-4
+    xStart = -2e-4
+    yStart = -2e-4
     xEnd = -xStart
     yEnd = -yStart
     count = Int(1e2)
-    tCount = Int(1e2)
+    tCount = Int(5e2)
 
     xData = range(xStart, xEnd, length=count)
     yData = range(yStart, yEnd, length=count)
 
-    dtSensor = 1.3e-14
+    dtSensor = 1e-14
     t = range(start=0, step=dtSensor, length=tCount)
     c = 3e8
     e0 = 8.8541878128e-12
     mu0 = 1.25663706212e-6
-    qe = 1.60217663e-19
+    qe = -1.60217663e-19
 
     sensor = createGridArray(xStart, xEnd, count, yStart, yEnd, count)
 
@@ -51,27 +50,12 @@ default(aspect_ratio=:auto)
 
     distSynced = (xSynced .^ 2 + ySynced .^ 2) .^ 0.5
 
-#=     
-        retard = true
 
-        if retard == true
-            p = contourf(xData, yData, permutedims(distSynced[1, :, :], [2, 1]), linewidth=0, aspect_ratio=:equal, colormap=:jet, levels=100,) #=ylims=[yStart,yEnd]./5=#
+    EFieldX, EFieldY = calculateRadiationField(xSynced, ySynced, distSynced, dtSensor)
 
-            for i in 1:count
-                p.series_list[1].plotattributes[:z] = permutedims(distSynced[i, :, :], [1, 2])
-                display(p)
-                sleep(1 / 10)
-            end
+    trace1 = scatter(x=t, y=EFieldX[:,50,1], name="X")
+    trace2 = scatter(x=t, y=EFieldY[:,50,1], name="Y")
 
-        else
-            p = contourf(xData, yData, permutedims(dist_data[1, :, :], [2, 1]), linewidth=0, aspect_ratio=:equal, colormap=:jet, levels=100,) #=ylims=[yStart,yEnd]./5=#
+    plot([trace1,trace2])
 
-            for i in 1:count
-                p.series_list[1].plotattributes[:z] = permutedims(dist_data[i, :, :], [1, 2])
-                display(p)
-                sleep(1 / 10)
-            end
-
-        end
-    =# 
 end
--- functions_detector.jl
+++ functions_detector.jl
@@ -33,17 +33,51 @@ function calculateRadiationField(xSynced::Array, ySynced::Array, distSynced::Arr
     aY = myDiffTime(vY) / dt
     absV = (vX .^ 2 + vY .^ 2) .^ 0.5
 
-    term11 = qe / 4 / pi / e0 * (1 .- absV .^ 2)
-    term12xnum = xSynced .- distSynced .* vY
-    term12ynum = ySynced .- distSynced .* vY
-    term12nom = (distSynced - myDotProduct(xSynced, ySynced, vX, vY)) .^ 3
-
+    term11 = qe / 4 / pi / e0 * (1 .- (absV / c) .^ 2)
+    term12xnum = xSynced .- distSynced .* vX / c
+    term12ynum = ySynced .- distSynced .* vY / c
+    termnom = (distSynced - myDotProduct(xSynced, ySynced, vX / c, vY / c)) .^ 3
+    term21 = qe * mu0 / 4
+    term2in = myCrossProduct(term12xnum, term12ynum, aX, aY)
+    term2outX, term2outY = myCrossProduct(xSynced, ySynced, term2in)
+    outX = @. (term11 * term12xnum + term21 * term2outX) / termnom
+    outY = @. (term11 * term12ynum + term21 * term2outY) / termnom
+    return outX, outY
 end
 
 function myDiffTime(timeData::Array)
-    return cat(1, zeros(size(timeData)[2:end]...), diff(timeData, dims=1), dims=1)
+    return cat(zeros(1, size(timeData)[2:end]...), diff(timeData, dims=1), dims=1)
 end
 
 function myDotProduct(X::Array, Y::Array, vX::Array, vY::Array)
     X .* vX .+ Y .* vY
+end
+
+function myCrossProduct(Ax::Array, Ay::Array, Bx::Array, By::Array)
+    return myLength(Ax, Ay) .* myLength(Bx, By) .* sin.(myAngle(Ax, Ay, Bx, By))
+end
+
+function myCrossProduct(Ax::Array, Ay::Array, Bz::Array)
+    outLen = myLength(Ax, Ay) .* Bz
+    outAng = mySingleAngle(Ay, Ay) .- pi ./ 2
+    outCPLX = @. outLen * exp(1im * outAng)
+    outX = @. real(outCPLX)
+    outY = @. imag(outCPLX)
+end
+
+function myAngle(Ax::Array, Ay::Array, Bx::Array, By::Array)
+    aCPLX = @. Ax + 1im * Ay
+    angA = @. angle(aCPLX)
+    bCPLX = @. Bx + 1im * By
+    angB = @. angle(bCPLX)
+    return angB .- angA
+end
+
+function myLength(Ax::Array, Ay::Array)
+    return @. (Ax^2 + Ay^2)^0.5
+end
+
+function mySingleAngle(Ax::Array, Ay::Array)
+    vecCPLX = @. Ax + 1im * Ay
+    return angle.(vecCPLX)
 end
